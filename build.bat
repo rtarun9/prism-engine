769c285b5cc7b1b0e6bc6aa40a59640cc055de7a
rtarun9 <rtarun0913@gmail.com>
2024-07-18 08:34:17 +0530

    Created basic win32 window and setup game loop

--- build.bat
+++ build.bat
@@ -1,7 +1,13 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
+set open_vs_debugger=0
+
 mkdir build
 pushd build
-cl /Zi ../src/main.c user32.lib
+cl /Zi ../src/main.c /link /SUBSYSTEM:windows user32.lib 
 popd
+
+IF %open_vs_debugger%==1 (
+    devenv build/main.exe
+)
--- src/main.c
+++ src/main.c
@@ -1,8 +1,72 @@
 #include "windows.h"
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+LRESULT CALLBACK window_proc(HWND window_handle, UINT message, WPARAM wparam, LPARAM lparam)
 {
-    MessageBox(0, "Hello, World!", "Test Message Box", MB_OK);
+    switch (message)
+    {
+    case WM_CLOSE: {
+        DestroyWindow(window_handle);
+    }
+    break;
+
+    case WM_DESTROY: {
+        PostQuitMessage(0);
+    }
+    break;
+
+    default: {
+        return DefWindowProc(window_handle, message, wparam, lparam);
+    }
+    break;
+    }
+
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR command_line, int show_command)
+{
+    // Create the window class, which defines a set of behaviours that multiple windows might have in common.
+    WNDCLASSA window_class = {};
+    window_class.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    window_class.lpfnWndProc = window_proc;
+    window_class.cbClsExtra = 0;
+    window_class.cbWndExtra = 0;
+    window_class.hInstance = instance;
+    window_class.lpszClassName = "BaseWindowClass";
+
+    if (!RegisterClassA(&window_class))
+    {
+        OutputDebugStringA("Failed to create window class.");
+    }
+
+    // Create a window.
+    HWND window_handle = CreateWindowA("BaseWindowClass", "prism-engine", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+                                       CW_USEDEFAULT, 1080, 720, NULL, NULL, instance, NULL);
+    if (window_handle == NULL)
+    {
+        OutputDebugStringA("Failed to create window.");
+    }
+
+    ShowWindow(window_handle, SW_SHOWNORMAL);
+
+    // Main game loop.
+    while (1)
+    {
+        MSG message = {};
+        if (PeekMessageA(&message, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&message);
+            DispatchMessage(&message);
+        }
+
+        // WM_QUIT message is posted when PostQuitMessage is called.
+        if (message.message == WM_QUIT)
+        {
+            break;
+        }
+    }
+
+    MessageBoxA(NULL, "Terminating window", "X", MB_OK);
 
     return 0;
 }
